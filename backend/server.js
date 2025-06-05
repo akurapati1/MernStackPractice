c6dccf8eef44db14ba3d7423265a19ce02b6fa7e
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-06-05 17:28:22 -0500

    Backend created successfully

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/slash', (req, res) => {
+  res.send('Budget Tracker API is running');
+});
+app.get('/', (req, res) => {
+    res.send('default route');
+  });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
